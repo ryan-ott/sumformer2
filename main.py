db4d718d03d6cfaacbcae97172bbb9b573e039bf
Ryan Ott <ott.r21@gmail.com>
2023-06-08 11:43:15 +0200

    Changed constant lr again

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import itertools
 import math
 import random
 import fire
@@ -76,7 +75,7 @@ def main(epochs=1, batch_size=16, lr=5e-4, sched="onecycle", emb_dim=512, max_le
     criterion = torch.nn.CrossEntropyLoss(ignore_index=PAD_TOKEN_ID, reduction='mean')  # * see without padding mask in decoder
     lr_schedules = {
         "linear": lr_scheduler.LinearLR(optimizer, start_factor=lr/10, end_factor=lr, total_iters=len(train_loader)*epochs),
-        "constant": lr_scheduler.ConstantLR(optimizer, factor=1, total_iters=0),  # an actual constant learning rate
+        "constant": lr_scheduler.ConstantLR(optimizer, factor=lr, total_iters=0),  # an actual constant learning rate
         "onecycle": lr_scheduler.OneCycleLR(optimizer, max_lr=lr, total_steps=len(train_loader)*epochs, pct_start=0.3, anneal_strategy="linear"),
         "invsqrt": lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda epoch: 1/math.sqrt(epoch) if epoch > 0 else 1),
         "cosinedecay": lr_scheduler.CosineAnnealingLR(optimizer, T_max=len(train_loader) * epochs)}
